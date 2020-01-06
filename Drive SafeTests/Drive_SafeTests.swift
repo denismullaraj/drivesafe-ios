fcc3e19a6de054bd606dafbf1394a4e30eaef900
Denis Mullaraj <denis.mullaraj@hotmail.com>
2020-01-06 17:23:38 +0100

    Setup unit testing environment

---
+++ Drive SafeTests/Drive_SafeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Drive_SafeTests.swift
+//  Drive SafeTests
+//
+//  Created by Denis Mullaraj on 06/01/2020.
+//  Copyright © 2020 Denis Mullaraj. All rights reserved.
+//
+
+import XCTest
+
+class Drive_SafeTests: XCTestCase {
+
+    func testVoid() {
+        XCTAssertTrue(true)
+    }
+
+}
